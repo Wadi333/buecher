44830d716071f60dfb2b387fbd7e9777a2f7f47d
Wadi <s83117@beuth-hochschule.de>
2020-11-07 10:45:02 +0100

    First step is done

---
+++ buecher_func.c
@@ -0,0 +1,121 @@
+/***********************************************************************
+ * buecher_func.c
+ * Sammlung von Funktionen zur B�cherlisten-Verwaltung
+ *
+ * Autor: H. Radners (Rumpfprogramm)
+ ***********************************************************************/
+
+#include "buecher_func.h"
+
+/***********************************************************************/
+
+void buecher_read(FILE *infile,
+                  Buch buecher[], int *num_buch, const int MAXBUCH,
+                  Autor autoren[], int *num_autor, const int MAXAUTOR,
+                  Verlag verlage[], int *num_verlag, const int MAXVERLAG) {
+   /* Liest das komplette Eingabe-file zeilenweise von (bereits
+    * ge�ffnetem) infile ein */
+
+   enum { MAXLL = 240 };   /* max. input line length */
+   char c, linebuf[MAXLL]; /* Einlesepuffer f�r eine Zeile */
+
+   fgets(linebuf, sizeof(linebuf), infile); /* 1x Kopfzeile ueberlesen */
+
+   /* Schleife ueber alle B�cher-Datens�tze, speichern */
+   while (fgets(linebuf, sizeof(linebuf), infile)) {
+
+      if (*num_buch >= MAXBUCH) {    /* Feldgroesse absichern */
+         fprintf(stderr,
+                 " *** Hinweis: Mehr als %d B�cher auf der Datei!\n", MAXBUCH);
+         break;
+      }
+
+      /* Zeilenl�nge absichern */
+      if (linebuf[strlen(linebuf) - 1] != '\n') {   /* Zeile zu lang! */
+         fprintf(stderr,
+                 " *** Hinweis: Zeile auf der Datei zu lang (>%d)! ", MAXLL);
+         fprintf(stderr, "Diese Zeile ignoriert:\n *** >%s...<\n", linebuf);
+         while ((c = fgetc(infile)) != EOF) { /* Rest der �berlangen Zeile ... */
+            if (c == '\n') break;             /* ... �berlesen bis NL od. EOF  */
+         }
+         continue;   /* diesen (unvollst�ndigen) Buch-Datensatz ignorieren */
+      }
+
+      /* dieses Buch mit Verweisen auf Autor und Verlag speichern */
+      buch_add(linebuf, buecher, num_buch,
+               autoren, num_autor, MAXAUTOR,
+               verlage, num_verlag, MAXVERLAG);
+   }
+
+}
+
+/***********************************************************************/
+
+void buch_add(char linebuf[],
+              Buch buecher[], int *num_buch,
+              Autor autoren[], int *num_autor, const int MAXAUTOR,
+              Verlag verlage[], int *num_verlag, const int MAXVERLAG) {
+   /* Erh�lt die eingelesene Eingabezeile eines Buch-Datensatzes,
+    * zerlegt diese am Trennzeichen in die Attribute und speichert diese.
+    * In der Buch-Struktur werden Zeiger auf dessen Autor- und Verlag-
+    * Element gespeichert.
+    */
+
+   const char *delim = ";\n";    /* CSV-Trennzeichen + NL! */
+   char* cp_autor ; 
+   char* cp_verlag ;
+   char* cp_isbn ;
+   unsigned short cp_erscheinungsjahr ;
+   /* Zeile zerlegen und Teile speichern */
+   /* Titel;Autor;Verlag;Erscheinungsjahr;ISBN */
+   buecher[*num_buch].titel            = strdup(strtok(linebuf, delim));
+   buecher[*num_buch].autor            = malloc(sizeof(Autor));
+   cp_autor                            = strdup(strtok(NULL, delim));
+   buecher[*num_buch].autor->name      = cp_autor ;
+   buecher[*num_buch].verlag           = malloc(sizeof(Verlag)) ;
+   cp_verlag                           = strdup(strtok(NULL, delim));
+   buecher[*num_buch].verlag->name     = cp_verlag;
+   cp_erscheinungsjahr                 = atoi(strdup(strtok(NULL, delim))) ;
+   buecher[*num_buch].erscheinungsjahr = cp_erscheinungsjahr ;
+   buecher[*num_buch].isbn             = malloc(sizeof(Buch));
+   cp_isbn                             = strdup(strtok(NULL, delim));
+   buecher[*num_buch].isbn             = cp_isbn ;   
+ 
+   /* Autor im bisherigen Autoren-Feld suchen, ggfs. neu anlegen,
+    * pointer darauf im akt. Buch speichern */
+   /* TODO */
+
+   /* Verlag im bisherigen Verlage-Feld suchen, ggfs. neu anlegen,
+    * pointer darauf im akt. Buch speichern */
+   /* TODO */
+
+   (*num_buch)++;
+
+}
+
+/***********************************************************************/
+
+void buecher_print(Buch buecher[], const int num_buch) {
+   /* Iteriert �ber B�cherliste zur Ausgabe */
+
+   unsigned int i;
+
+   for (i = 0; i < num_buch; i++) {
+      printf("%3d: ", i + 1);
+      buch_print(&(buecher[i]));
+   }
+   printf("\n");
+}
+
+/***********************************************************************/
+
+void buch_print(const Buch *b) {
+   /* Gibt ausgew�hlte Attribute eines Buches formatiert aus */
+
+   printf("%s (%d): %s\n",
+          (b->autor != NULL ? b->autor->name : "*unbekannt*"),
+          b->erscheinungsjahr, b->titel);
+}
+
+/***********************************************************************/
+
