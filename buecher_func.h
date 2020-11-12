94551ddb827f97cc3e19bb08abd65fdd169b5b31
Wadi333 <63963508+Wadi333@users.noreply.github.com>
2020-11-12 18:49:35 +0100

    Update buecher_func.h

--- buecher_func.h
+++ buecher_func.h
@@ -2,7 +2,7 @@
 #define BUECHER_FUNC_H
 /***********************************************************************
  * buecher_func.h
- * Headerfile der Sammlung von Funktionen zur Bcherlisten-Verwaltung
+ * Headerfile der Sammlung von Funktionen zur B�cherlisten-Verwaltung
  *
  * Autor: H. Radners (Rumpfprogramm)
  ***********************************************************************/
@@ -13,13 +13,13 @@
 /* Struktur einer Autor-Definition */
 typedef struct {
    char *name;                   /* Zeiger auf vollstdg. Namen dieses Autors */
-   unsigned short anz_buecher;   /* Anzahl der Bcher dieses Autors */
+   unsigned short anz_buecher;   /* Anzahl der B�cher dieses Autors */
 } Autor;
 
 /* Struktur einer Verlag-Definition */
 typedef struct {
    char *name;                   /* Zeiger auf vollstdg. Namen dieses Verlags */
-   unsigned short anz_buecher;   /* Anzahl der Bcher dieses Verlags */
+   unsigned short anz_buecher;   /* Anzahl der B�cher dieses Verlags */
 } Verlag;
 
 /* Struktur einer Buch-Definition */
@@ -37,14 +37,14 @@ void buecher_read(FILE *infile,
                   Autor autoren[], int *num_autor, const int MAXAUTOR,
                   Verlag verlage[], int *num_verlag, const int MAXVERLAG);
 /* Liest das komplette Eingabe-file zeilenweise von (bereits
- * geffnetem) infile ein */
+ * ge�ffnetem) infile ein */
 
 
 void buch_add(char linebuf[],
               Buch buecher[], int *num_buch,
               Autor autoren[], int *num_autor, const int MAXAUTOR,
               Verlag verlage[], int *num_verlag, const int MAXVERLAG);
-/* Erhlt die eingelesene Eingabezeile eines Buch-Datensatzes,
+/* Erh�lt die eingelesene Eingabezeile eines Buch-Datensatzes,
  * zerlegt diese am Trennzeichen in die Attribute und speichert diese.
  * In der Buch-Struktur werden Zeiger auf dessen Autor- und Verlag-
  * Element gespeichert.
@@ -52,21 +52,15 @@ void buch_add(char linebuf[],
 
 
 void buecher_print(Buch buecher[], const int num_buch);
-/* Iteriert ber Bcherliste zur Ausgabe */
+/* Iteriert �ber B�cherliste zur Ausgabe */
 
 
 void buch_print(const Buch *b);
-/* Gibt ausgewhlte Attribute eines Buches formatiert aus */
+/* Gibt ausgew�hlte Attribute eines Buches formatiert aus */
 
-
-void print_top_Verlage(Verlag* verlage , int NUMTOP ) ; 
-void print_top_Autoren(Autor* autoren , int NUMTOP ) ;  
-
-int counter(char *str, Buch m_buecher[], int N) ;       
-int counter2(char *str, Buch m_buecher[], int N) ;
-
-
-int compare1 (const void * a, const void * b) ;       
-int compare2 (const void * a, const void * b) ;
+Autor *autor_link(Autor autoren[], int *num_autor, char *autorname);
+Verlag *verlag_link(Verlag verlage[], int *num_verlage, char *verlagname);
+int cmp_autor_anzbuch(const void *p1, const void *p2) ;
+void perm_autoren(int *num_autor, Autor autoren[]) ;
 
 #endif
